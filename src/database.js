e7f755db1a1b627420ed6dd2f1fff42f71907909
iwilsonq <iwilsonq@gmail.com>
2017-02-28 14:49:58 -0800

    wiring up facebook-lite

---
+++ src/database.js
@@ -0,0 +1,20 @@
+import sqlite3 from 'sqlite3';
+
+import * as tables from './tables';
+
+export const db = new sqlite3.Database('./db.sqlite');
+
+export const getSql = query => {
+  return new Promise((resolve, reject) => {
+    console.log('TEXT:', query.text);
+    console.log('VALUES:', query.values);
+    db.all(query.text, query.values, (err, rows) => {
+      console.log(err, rows);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+};
